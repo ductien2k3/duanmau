7cc85fe18b3d6bd333925a2027e348004071e4ca
ductien2k3 <124386506+ductien2k3@users.noreply.github.com>
2023-10-11 20:57:06 +0700

    Delete duanmau directory

--- duanmau/view/gioithieu.php
+++
@@ -1,6 +0,0 @@
-
-<div class="mb">
-    <div class="box_title">Giới thiệu</div>
-    <div class="box_content">
-    </div>
-</div>
--- duanmau/view/lienhe.php
+++
@@ -1,6 +0,0 @@
-<div class="mb">
-    <div class="box_title">Liên hệ</div>
-        <div class="box_content">
-                
-        </div>
-</div>
